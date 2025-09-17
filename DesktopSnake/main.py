1c49210642213179e8af4059e45683c9913af58d
Lazykangeroo <fvantonder00@gmail.com>
2025-09-17 21:54:45 +0200

    Keyboard input
    Added keyboard input checking, along with a gameloop - While not key x pressed.
    
    Also added validation for data lists. This validation file will be expanded on but could also be removed later when development is finished.

--- DesktopSnake/main.py
+++ DesktopSnake/main.py
@@ -1,13 +1,40 @@
-import os
 import pyautogui as gui
+import keyboard as kb
 import time
 
-head = {'x':50,'y':50}
-tail = []
-duration = 0.12
-direction_shift = {'x':50,'y':50}
+#! Validation package
+from validation import ErrMsg
+errmsg = ErrMsg()
 
-def move_icons()
-    gui.moveTo(x=50,y=50)
-    gui.dragTo(x=100,y=50, duration=duration,button='left')
-    
+head = {
+    'x' : 50,
+    'y' : 50
+    }
+tail = [head]
+duration = 0.12
+grid_size = 50
+
+def move_icons(from_x,from_y,to_x,to_y):
+    gui.moveTo(x=from_x,y=from_y) #? move to end of tail
+    gui.dragTo(x=to_x,y=to_y, duration=duration,button='left') #? drag icon to the new front or 'head'
+
+def input(x,y):
+    if kb.is_pressed('w'): #* up
+        return {'x':x,'y':y-grid_size}
+    if kb.is_pressed('s'): #* down
+        return {'x':x,'y':y+grid_size}
+    if kb.is_pressed('a'): #* left
+        return {'x':x-grid_size,'y':y}
+    if kb.is_pressed('d'): #* right
+        return {'x':x+grid_size,'y':y}
+
+while not kb.is_pressed('x'):
+    time.sleep(0.01) #? as to not overload CPU with calculations
+    new_head_pos = input(head['x'],head['y']) # using key input to determine new head
+    #! here the snake doesnt move on it's own yet
+
+    if new_head_pos is None:
+        errmsg.dataErr("Data value None returned instead of expected type of { x:int, y:int }", 32)
+
+    #* movement of icons
+    move_icons(tail[-1]['x'],tail[-1]['y'],new_head_pos['x'],new_head_pos['y']) # type: ignore
---
+++ validation.py
@@ -0,0 +1,10 @@
+## File is merely for my own debugging and error handling during production
+## This mean the link between the files could be disconnected if so decided
+## This isn't incurraged to disallow any accidental errors but feel free
+
+class ErrMsg:
+    def __init__(self) -> None:
+        pass
+
+    def dataErr(self, text, line):
+        print(f"[!] Data error: {text} ({str(line)})")
