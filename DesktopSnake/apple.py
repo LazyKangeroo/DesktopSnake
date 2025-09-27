35c9152524226b226471c626b87657c4e52bd6bd
Lazykangeroo <fvantonder00@gmail.com>
2025-09-27 22:10:57 +0200

    Apple spawning
    The spawning interval and recording of icons to uses for apples completed.
    
    Having problems with the moving of icons to spawning point - especially when a directional input is being entered.
    Possible fix is to set a delay between the two functionalities.

---
+++ DesktopSnake/apple.py
@@ -0,0 +1,70 @@
+import random
+import pyautogui as gui
+import keyboard as kb
+
+class Apple:
+    def __init__(self,stored_pos,all_stored_pos,tail,apples):
+        self.position = {'x': None, 'y': None}
+        self.stored_pos = stored_pos
+
+        self.spawn(tail,apples,all_stored_pos)
+        self.move_to_spawn()
+
+    def spawn(self,tail,apples,all_stored_pos):
+        screen_width, screen_height = gui.size()
+        grid_x = 100
+        grid_y = 120
+        apple_x = random.randint(0, screen_width // grid_x) * grid_x
+        apple_y = random.randint(0, screen_height // grid_y) * grid_y
+
+        if self.check_collision(tail, apples,all_stored_pos): #? ensure no colliction with body of snake and other apples
+            self.spawn(tail, apples,all_stored_pos) #! try again
+            return
+
+        self.position = {'x': apple_x, 'y': apple_y}
+
+    #! moving apple to spawn position from stored position
+    def move_to_spawn(self):
+        gui.moveTo(x=self.stored_pos['x'], y=self.stored_pos['y'])
+        gui.dragTo(x=self.position['x'], y=self.position['y'], duration=0.5, button='left')
+
+
+    #! Ensure no colliction with body of snake and other apples
+    def check_collision(self, tail, apples,all_stored_pos):
+        for segment in tail:
+            if self.position['x'] == segment['x'] and self.position['y'] == segment['y']:
+                return True
+        for apple in apples:
+            if self.position['x'] == apple.position['x'] and self.position['y'] == apple.position['y']:
+                return True
+        for pos in all_stored_pos:
+            if self.position['x'] == pos['x'] and self.position['y'] == pos['y']:
+                return True
+        return False
+
+##! Function to get apple positions from user
+def get_apple_position():
+    positions = []
+    formatted_positions = []
+    amnt = 0
+
+    while True:
+        #! Wait for r to be pressed
+        if kb.is_pressed("r"):
+            pos = gui.position()
+            positions.append(pos)
+            amnt += 1
+            print(f"   [{str(amnt)}] Recorded: {pos}")
+
+            gui.sleep(0.2)#? prevent multiple recordings from one click
+
+        #! Stop when Enter is pressed
+        if kb.is_pressed("enter"):
+            print("> Recording stopped.")
+            break
+
+    #? Formatting positions for easy use
+    for p in positions:
+        formatted_positions.append({'x': p[0], 'y': p[1]})
+
+    return formatted_positions # set of recorded positions
--- DesktopSnake/main.py
+++ DesktopSnake/main.py
@@ -2,6 +2,8 @@ import pyautogui as gui
 import keyboard as kb
 import time
 
+from apple import Apple, get_apple_position
+
 ####! Fail save
 gui.FAILSAFE = True
 
@@ -12,10 +14,16 @@ screen_width, screen_height = gui.size() #* get screen size
 grid_x = 100
 grid_y = 120
 
-default_head_pos = {'x' : 30+grid_x*4,'y' : 50} #* initial head position
+default_head_pos = {'x' : 30,'y' : 50} #* initial head position
 new_head_pos = {'x': None, 'y': None}
-tail = [default_head_pos,{'x' : 30+grid_x*3,'y' : 50},{'x' : 30+grid_x*2,'y' : 50},{'x' : 30+grid_x*1,'y' : 50},{'x' : 30+grid_x*0,'y' : 50}] #* initial tail positions
+tail = [default_head_pos] #* initial tail positions
 direction = {'x': grid_x, 'y': 0} #* initial direction
+
+#! Apple setup
+apples = [] #* list to hold apples
+apple_stored_pos = [] #* position to store apple spawn position
+apple_spawn_interval = 20 #* spawn an apple every 15 loops
+apple_timer = 0 #* timer to track apple spawns intervals
 
 ##! Function to move icons
 def move_icons(from_x,from_y,to_x,to_y):
@@ -50,9 +58,15 @@ def duration_manager(amnt_icons):
     duration = DURATION_INTERVAL * amnt_icons
 
 ##! Initial setup
-print('Place one icon at the top left corner of the screen (This will be the head)')
+print('-----Desktop Snake-----')
+print('Please hover over all the icons you want to use for the snake/apples.\nPress "r" to record and "enter" to continue...')
+
+apple_stored_pos = get_apple_position() # get possible apple positions from user
+
+print('\nPlace one icon at the top left corner of the screen (This will be the head)')
 print('Use W A S D to move the snake')
 print('Press x to exit')
+
 print('Starting in 3 seconds...')
 time.sleep(3) #? give user time to switch to desired window
 
@@ -87,3 +101,13 @@ while not kb.is_pressed('x'): #* press 'x' to exit
     move_icons(tail[-1]['x'],tail[-1]['y'],new_head_pos['x'],new_head_pos['y'])
 
     tail.pop() # remove the last element of the list
+
+    #! Init Apple at random position at set intervals
+    if apple_timer == 0 or apple_timer == apple_spawn_interval: # spawn apple at start or at set intervals
+        if len(apple_stored_pos) > 0: #? check if there are stored apple positions left
+            apples.append(Apple(stored_pos=apple_stored_pos[-1],tail=tail,apples=apples,all_stored_pos=apple_stored_pos)) #* spawn new apple at random position
+        else: #? no more apple stored
+            print('\n - No more stored apples! -\n\n----YOU WON!----')
+            break
+        apple_timer = 0 # reset timer
+    apple_timer += 1 # increment timer
